a6b45e53d80b950fcab2c3f4ac5f04c324bb020b
unknown <tuanbinh.35@gmail.com>
2020-09-07 23:47:17 +0700

    Updates

---
+++ precache-manifest.99892fc9f7a294080824748b60f2a5c6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5b659b1a24fb0df5bbf27e2bb4f8dc3f",
+    "url": "/dangtuanbinh/Dressing-room.git/index.html"
+  },
+  {
+    "revision": "75770d3d6e3c13cbc34d",
+    "url": "/dangtuanbinh/Dressing-room.git/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "54ef868dcea47e8fd765",
+    "url": "/dangtuanbinh/Dressing-room.git/static/css/main.e5f89e72.chunk.css"
+  },
+  {
+    "revision": "75770d3d6e3c13cbc34d",
+    "url": "/dangtuanbinh/Dressing-room.git/static/js/2.b93dd41d.chunk.js"
+  },
+  {
+    "revision": "a7b079a06d99e0bdddae2cf2b771ca7a",
+    "url": "/dangtuanbinh/Dressing-room.git/static/js/2.b93dd41d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "54ef868dcea47e8fd765",
+    "url": "/dangtuanbinh/Dressing-room.git/static/js/main.465daa77.chunk.js"
+  },
+  {
+    "revision": "cb6bfd6430ba44a27d17",
+    "url": "/dangtuanbinh/Dressing-room.git/static/js/runtime-main.eaef7d38.js"
+  }
+]);
